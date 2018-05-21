c12f98eca0ab8796c865535c5efa1f11098be943
Sara Foresti <Sara.foresti@hotmail.com>
2018-05-21 18:32:54 +0100

    controllers created

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,12 @@
 class Bike < ApplicationRecord
   belongs_to :user
+
+  # validates :type, presence: true
+  # validates :size, presence: true
+  # validates :address, presence: true
+  # validates :price, presence: true, numericality: true
+
 end
+
+
+# inclusion: { in: %w(road mountain hybrid city lady cruiser cargo)}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  has_many :bikes
+  has_many :bookings
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
 end
