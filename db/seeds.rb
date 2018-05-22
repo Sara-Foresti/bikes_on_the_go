de23fb9f1381c993b86ed79344f5b7f0f2b31303
Sam Ricotta <samanthalricotta@gmail.com>
2018-05-22 17:10:42 +0100

    additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  arthur = User.create!(email: "arthur@lewagon.com", password: "123456")
+  user = User.create!(email: "arthur@lewagon.com", password: "123456")
+
   Bike.create!(
     name: 'Kids Mountain Bike in Good Condition',
     description: 'Hi I have kids mountain bike in good working order 20 inch wheels Good tyres, gears and brakes Recently checked and serviced As you can see from the pictures the bike is in good condition',
     size: 'kids - 10 years old',
     price: 40,
     address: '138 Kingsland Rd, London',
-    user: arthur
+    user: user
   )
 
+Bike.create!(
+    name: 'Green fixie',
+    description: 'Working bike, have had it for 1 year only and it is working well',
+    size: 'Womens',
+    price: 40,
+    address: '138 Kingsland Rd, London',
+    user: user
+  )
 
+Bike.create!(
+    name: 'Green fixie',
+    description: 'Working bike, have had it for 1 year only and it is working well',
+    size: 'Womens',
+    price: 40,
+    address: '138 Kingsland Rd, London',
+    user: user
+  )
+
+Bike.create!(
+    name: 'Green fixie',
+    description: 'Working bike, have had it for 1 year only and it is working well',
+    size: 'Womens',
+    price: 40,
+    address: '138 Kingsland Rd, London',
+    user: user
+  )
+
+Bike.create!(
+    name: 'Green fixie',
+    description: 'Working bike, have had it for 1 year only and it is working well',
+    size: 'Womens',
+    price: 40,
+    address: '138 Kingsland Rd, London',
+    user: user
+  )
