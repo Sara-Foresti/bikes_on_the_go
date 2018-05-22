82c70f5c45642abc70e2acf90ad078cdc0788efd
Natalia Raffaele <natalia.rafe@gmail.com>
2018-05-22 15:53:34 +0100

    new and create somme css added

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -11,6 +11,22 @@ class BikesController < ApplicationController
     @bike = Bike.new
   end
 
+  def create
+    @bike = Bike.new(bike_params)
+    if @bike.save!
+      redirect_to bikes_path
+    else
+      render :new
+    end
+  end
+
   def edit
   end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:name, :description, :size, :type, :price, :address)
+  end
+
 end
