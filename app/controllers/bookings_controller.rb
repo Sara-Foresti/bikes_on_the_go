a3399cc5a8d7206525e1f1aee57e63908736decb
Sam Ricotta <samanthalricotta@gmail.com>
2018-05-23 12:58:32 +0100

    nested routes, and done create method for bikes controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,21 @@
 class BookingsController < ApplicationController
 
   def create
+  @bike = Bike.find(params[:bike_id])
+  @booking = Booking.new(booking_params)
+  @booking.bike = @bike
+  if @booking.save
+    redirect_to bike_path(@bike)
+  else
+    render :new
+  end
 
+  private
+
+
+
+  def booking_params
+    params.require(:bookings).permit(:status)
   end
 
 end
