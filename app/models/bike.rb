6ba9281647838f1ba59341881eac95ef19d8d258
Natalia Raffaele <natalia.rafe@gmail.com>
2018-05-24 17:01:55 +0100

    map added to show index

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,6 +3,8 @@ class Bike < ApplicationRecord
   has_many :bookings
   has_many :bookers, through: :bookings, source: :user
   mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   # validates :category, presence: true
   # validates :size, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_121548) do
+ActiveRecord::Schema.define(version: 2018_05_24_114500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_05_23_121548) do
     t.string "name"
     t.string "description"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
